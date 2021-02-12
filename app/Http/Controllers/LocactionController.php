4d0f50fe1b8bc2320b7fba918aa699952cce878d
dreambaaz <dreambaaz.bd@gmail.com>
2021-02-13 01:29:53 +0600

    Location and Designation controller, model, migration, seeder has been added

---
+++ app/Http/Controllers/LocactionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocactionController extends Controller
+{
+    //
+}
---
+++ database/factories/LocactionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Locaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LocactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Locaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
